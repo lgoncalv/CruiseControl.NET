19d64384510bb831c9b36b31959b242a8bc59a1f
Luciano Goncalves <lgoncalv@gmail.com>
2013-09-11 13:47:02 +0100

    NEW: Configure Windows Login

---
+++ project/CCTrayLib/Security/ConfigureWinLogin.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ThoughtWorks.CruiseControl.CCTrayLib.Security
+{
+    partial class ConfigureWinLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigureWinLogin));
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tPassword = new System.Windows.Forms.TextBox();
+            this.tDomain = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.CausesValidation = false;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCancel.Location = new System.Drawing.Point(197, 90);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 24);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Enabled = false;
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnOK.Location = new System.Drawing.Point(116, 91);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 24);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password:";
+            // 
+            // tPassword
+            // 
+            this.tPassword.Location = new System.Drawing.Point(76, 64);
+            this.tPassword.Name = "tPassword";
+            this.tPassword.Size = new System.Drawing.Size(196, 20);
+            this.tPassword.TabIndex = 1;
+            this.tPassword.UseSystemPasswordChar = true;
+            this.tPassword.TextChanged += new System.EventHandler(this.tPassword_TextChanged);
+            // 
+            // tDomain
+            // 
+            this.tDomain.Location = new System.Drawing.Point(76, 38);
+            this.tDomain.Name = "tDomain";
+            this.tDomain.ReadOnly = true;
+            this.tDomain.Size = new System.Drawing.Size(196, 20);
+            this.tDomain.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Domain:";
+            // 
+            // tUsername
+            // 
+            this.tUsername.Location = new System.Drawing.Point(76, 12);
+            this.tUsername.Name = "tUsername";
+            this.tUsername.ReadOnly = true;
+            this.tUsername.Size = new System.Drawing.Size(196, 20);
+            this.tUsername.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Username:";
+            // 
+            // ConfigureWinLogin
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(284, 123);
+            this.Controls.Add(this.tUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tDomain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ConfigureWinLogin";
+            this.Text = "Configure Win Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tPassword;
+        private System.Windows.Forms.TextBox tDomain;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tUsername;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ project/CCTrayLib/Security/ConfigureWinLogin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ThoughtWorks.CruiseControl.CCTrayLib.Security
+{
+    public partial class ConfigureWinLogin : Form
+    {
+        public ConfigureWinLogin(string userName, string domain)
+        {
+            InitializeComponent();
+            tUsername.Text = userName;
+            tDomain.Text = domain;
+        }
+
+        public string Password
+        {
+            get { return tPassword.Text; }
+        }
+
+        public event EventHandler PasswordSet;
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (PasswordSet != null) PasswordSet(this, EventArgs.Empty);
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void tPassword_TextChanged(object sender, EventArgs e)
+        {
+            btnOK.Enabled = !string.IsNullOrEmpty(tPassword.Text);
+        }
+    }
+}
